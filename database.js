fd1e31a17b76046f57cba04a861e751116274517
IkyVold <rpunk8514@gmail.com>
2026-08-12 22:56:12 +0700

    yes

--- database.js
+++ database.js
@@ -1,9 +1,8 @@
-// database.js
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || '127.0.0.1',
-    user: process.env.DB_USER,
+    user: process.env.DB_USER || 'bk_backend',
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME || 'bk_system',
     waitForConnections: true,
